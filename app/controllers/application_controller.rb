791fd3ba5f3023a4f7b0d8a14cb9ad8c44926c3a
Marvin Pafla <marvin.pafla@outlook.de>
2017-03-12 01:32:46 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render "website/home"
+    render html: "<h1>Welcome to our care giving app!</h1>"
   end
 end
