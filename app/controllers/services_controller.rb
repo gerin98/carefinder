855045702137e4e7fd34aa2852a5b357036ed53f
Marvin Pafla <marvin.pafla@outlook.de>
2017-03-12 07:36:52 +0000

    Workflow

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,36 @@
 class ServicesController < ApplicationController
   def new
+    @user = User.find(params[:id])
+    @service = Service.new
+
   end
 
   def show
+
+    @service = Service.find(params[:id])
+    @user = User.find(@service.user_id)
+    
+    
   end
 
   def index
+    @services = Service.all
   end
+  
+   def create
+    @user = User.find(params[:id])
+    @service = @user.services.build()
+    @service.purpose = params[:purpose]
+    @service.comment = params[:comment]
+    if @service.save
+      flash[:success] = "Service created"
+      redirect_to @service
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+
 end
